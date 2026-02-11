481fab07bde714a912a4707c032e2dfaa81a0b4e
hamidwise1 <hamidwise1@gmail.com>
2026-02-11 19:40:00 +0330

    add dotenv lib

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 import { Hono } from "hono";
+import { load } from "@std/dotenv";
+
+// Load enviroment variables
+await load({
+  export: true,
+});
 
 const app = new Hono();
-
 // Middleware
 app.use(async (_, next) => {
   console.log("middleware");
@@ -13,9 +18,11 @@ app.get("/", (c) => {
   return c.html("<h1>Hello World</h1>");
 });
 
+// Start server
+const serverPort = parseInt(Deno.env.get("PORT") || "8000");
 Deno.serve(
   {
-    port: 8080,
+    port: serverPort,
   },
   app.fetch,
 );
