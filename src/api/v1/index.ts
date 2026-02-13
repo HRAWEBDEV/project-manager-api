f533142e7e08c1aee4764aec5885290e68cdd4c7
hamidwise1 <hamidwise1@gmail.com>
2026-02-13 11:44:06 +0330

    add zod response errors

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from "hono";
 import { router as organizationsRouter } from "./routes/organizations.ts";
 import { NotFoundError } from "../../db/utils/NotFound.ts";
+import { ZodError } from "zod";
 
 const v1Routes = new Hono().basePath("/v1");
 v1Routes.route("/", organizationsRouter);
 
 v1Routes.onError((err, c) => {
-  console.log(err);
+  if (err instanceof ZodError) {
+    return c.json({ status: "failed", message: err.message }, 400);
+  }
   if (err instanceof NotFoundError) {
     return c.json({ status: "failed", message: err.message }, 404);
   }
