00034f49d4aa3e8fe3404a8202d10cfd3b522bb9
hamidwise1 <hamidwise1@gmail.com>
2026-02-17 12:07:59 +0330

    add organizations members schema

---
+++ src/db/v1/schemas/organizationsMembers.ts
@@ -0,0 +1,13 @@
+import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { organizations } from "./organizations.ts";
+import { users } from "./users.ts";
+import { trackChanges } from "@src/db/utils/trackChanges.ts";
+
+const organizationMembers = pgTable("organization_members", {
+  id: uuid().primaryKey().defaultRandom(),
+  organizationId: uuid("organization_id").references(() => organizations.id),
+  userId: uuid("user_id").references(() => users.id),
+  ...trackChanges,
+});
+
+export { organizationMembers };
