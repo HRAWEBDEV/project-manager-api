e0aed5875a85463f04fadf4bbe4d1868000a015a
hamidwise1 <hamidwise1@gmail.com>
2026-02-17 12:19:11 +0330

    add roles schema

---
+++ src/db/v1/schemas/roles.ts
@@ -0,0 +1,13 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { organizations } from "./organizations.ts";
+import { trackChanges } from "../../utils/trackChanges.ts";
+
+const roles = pgTable("roles", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  organizationId: uuid("organization_id").references(() => organizations.id),
+  title: text().notNull(),
+  description: text().notNull(),
+  ...trackChanges,
+});
+
+export { roles };
