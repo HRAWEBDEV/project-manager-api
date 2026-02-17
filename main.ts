1a7ed6199188630b2e721f4129d320e11180152c
hamidwise1 <hamidwise1@gmail.com>
2026-02-17 11:26:19 +0330

    add static files and cors and secure headers

--- main.ts
+++ main.ts
@@ -1,16 +1,26 @@
 import { Hono } from "hono";
 import { secureHeaders } from "hono/secure-headers";
+import { cors } from "hono/cors";
 import { closeConnection, testConnection } from "@src/db/connect.ts";
 import { v1Routes } from "@src/api/v1/index.ts";
+import { serveStatic } from "hono/deno";
 
 const app = new Hono();
 const api = new Hono().basePath("/api");
 //
-// app.use(secureHeaders());
 // Routes
 api.route("/", v1Routes);
 app.route("/", api);
 
+// set secure header
+app.use(secureHeaders());
+// set cors setup
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
+// set static file server
+app.use("/static/*", serveStatic({ root: "./" }));
 // Start server
 const serverPort = parseInt(Deno.env.get("PORT") || "8000");
 
