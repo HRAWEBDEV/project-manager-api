04f6c02fa3b1dacfd2cb1b7ff3a08d57fb2fc736
hamidwise1 <hamidwise1@gmail.com>
2026-02-17 12:55:10 +0330

    add projects schema

---
+++ src/db/v1/schemas/projects.ts
@@ -0,0 +1,15 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { organizations } from "./organizations.ts";
+import { organizationMembers } from "./organizationsMembers.ts";
+import { trackChanges } from "../../utils/trackChanges.ts";
+
+const projects = pgTable("projects", {
+  id: uuid().defaultRandom().primaryKey(),
+  name: text().notNull(),
+  description: text(),
+  organizationId: uuid("organization_id").references(() => organizations.id),
+  createdBy: uuid("created_by").references(() => organizationMembers.id),
+  ...trackChanges,
+});
+
+export { projects };
