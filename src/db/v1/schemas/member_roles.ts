7c711dea3a1a57d8dc59a0c67a9d1cd4bf3f2d02
hamidwise1 <hamidwise1@gmail.com>
2026-02-19 16:54:23 +0330

    update users id

--- src/db/v1/schemas/member_roles.ts
+++ src/db/v1/schemas/member_roles.ts
@@ -1,12 +1,12 @@
 import { pgTable, uuid } from "drizzle-orm/pg-core";
 import { roles } from "./roles.ts";
-import { organizationMembers } from "./organizationsMembers.ts";
+import { users } from "./users.ts";
 import { trackChanges } from "../../utils/trackChanges.ts";
 
 const memberRoles = pgTable("member_roles", {
   id: uuid().defaultRandom().primaryKey(),
   roleId: uuid("role_id").notNull().references(() => roles.id),
-  memberId: uuid("member").notNull().references(() => organizationMembers.id),
+  memberId: uuid("member").notNull().references(() => users.id),
   ...trackChanges,
 });
 
--- src/db/v1/schemas/projects.ts
+++ src/db/v1/schemas/projects.ts
@@ -1,6 +1,6 @@
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { organizations } from "./organizations.ts";
-import { organizationMembers } from "./organizationsMembers.ts";
+import { users } from "./users.ts";
 import { trackChanges } from "../../utils/trackChanges.ts";
 
 const projects = pgTable("projects", {
@@ -10,9 +10,7 @@ const projects = pgTable("projects", {
   organizationId: uuid("organization_id").notNull().references(() =>
     organizations.id
   ),
-  createdBy: uuid("created_by").notNull().references(() =>
-    organizationMembers.id
-  ),
+  createdBy: uuid("created_by").notNull().references(() => users.id),
   ...trackChanges,
 });
 
