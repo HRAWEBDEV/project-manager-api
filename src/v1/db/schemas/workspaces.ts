79ea68dae076dc9c45c4d9ead8fc6336d26dada6
HRAWEBDEV <hrawebdev@gmail.com>
2026-07-12 08:57:09 +0330

    add workspace table

---
+++ src/v1/db/schemas/workspaces.ts
@@ -0,0 +1,25 @@
+import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { organizations } from "./organizations";
+import { users } from "./users";
+import { trackChanges } from "../utils/trackChanges";
+
+type Workspace = typeof workspaces.$inferSelect;
+type InsertWorkspace = typeof workspaces.$inferInsert;
+
+const workspaces = pgTable("workspaces", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  organizationId: uuid("organization_id")
+    .notNull()
+    .references(() => organizations.id, {
+      onDelete: "cascade",
+    }),
+  name: varchar("name", { length: 100 }).notNull(),
+  slug: varchar("slug", { length: 150 }).notNull().unique(),
+  createdBy: uuid("created_by")
+    .notNull()
+    .references(() => users.id),
+  ...trackChanges,
+});
+
+export type { Workspace, InsertWorkspace };
+export { workspaces };
