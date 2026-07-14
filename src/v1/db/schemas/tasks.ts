af94adadf0fd02df108527509e2757239865ca36
HRAWEBDEV <hrawebdev@gmail.com>
2026-07-14 17:12:50 +0330

    add tasks organization member id col

--- src/v1/db/schemas/tasks.ts
+++ src/v1/db/schemas/tasks.ts
@@ -8,6 +8,7 @@ import {
 } from "drizzle-orm/pg-core";
 import { projects } from "./projects";
 import { trackChanges } from "../utils/trackChanges";
+import { organizationMembers } from "./organizationMembers";
 
 const tasks = pgTable(
   "tasks",
@@ -20,6 +21,9 @@ const tasks = pgTable(
     endAt: timestamp("end_at"),
     completedAt: timestamp("completed_at"),
     parentTaskId: uuid("parent_task_id"),
+    createdBy: uuid("created_by").references(() => organizationMembers.id, {
+      onDelete: "set null",
+    }),
     ...trackChanges,
   },
   (table) => [
